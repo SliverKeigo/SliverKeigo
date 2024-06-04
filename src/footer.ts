1d599960c20aa113389a899972634d1dc99eb58f
Keigo <112426853+SliverKeigo@users.noreply.github.com>
2024-06-04 12:42:20 +0800

    Update footer.ts

--- src/footer.ts
+++ src/footer.ts
@@ -2,6 +2,7 @@ import { timeZone } from "./config"; // 'Asia/Shanghai'
 
 export async function getFooter() {
   return `
+  <p align="center">
 </br>
 最近一次刷新于：${new Date().toLocaleString(undefined, {
     timeStyle: "medium",
